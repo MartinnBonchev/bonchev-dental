f4a31841119b5f2b61972df80f983d0f9ac9c0d2
Martin Bonchev <martin.bonchevv@gmail.com>
2022-10-13 00:17:08 +0300

    feat: theme configuration with white and black

---
+++ src/styles/theme.ts
@@ -0,0 +1,13 @@
+import type { MantineThemeOverride } from "@mantine/core";
+
+const theme: MantineThemeOverride = {
+  colorScheme: "light",
+  primaryColor: "white",
+  defaultRadius: "md",
+  headings: {
+    fontFamily:
+      "Inter, -apple-system , BlinkMacSystemFont, Segoe UI, Roboto , Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji",
+  },
+};
+
+export default theme;
